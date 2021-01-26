1a2e5ad20c2db2f87adb0d058f5cd2c6acf35045
adele <adele.r87@gmail.com>
2021-01-26 14:24:23 +0100

    bonus

--- vue-js/script.js
+++ vue-js/script.js
@@ -1,8 +1,9 @@
 var app = new Vue({
  el: '#root',
  data:{
+   messageInput:'il valore che passerò sarà una stringa',
    messageH1:'ciao! sopravvivi a vue !',
-   image:'img/vue.png'
+   image:'img/vue.png',
  }
 });
 
